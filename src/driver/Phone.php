89220257f81e3a53253e5285723ced0c7f42cf9f
raphaeu <raphaeu.aguiar@gmail.com>
2020-10-07 19:50:18 -0300

    rest tts

--- src/driver/Phone.php
+++ src/driver/Phone.php
@@ -6,20 +6,18 @@
  * Time: 12:52
  */
 namespace multiSendMessage\driver;
-use providers\Provider;
+use GuzzleHttp\Client;
 
 class Phone extends ChannelAbstract implements ChannelInterface
 {
     private $shellScriptPath;    
     private $path;    
-    private $providerName;
     private $repeat;
 
-    public function __construct($shellScriptPath, $path, $providerName, $repeat=3)
+    public function __construct($shellScriptPath, $path, $repeat=3)
     {
         $this->shellScriptPath = $shellScriptPath;
         $this->path = $path;
-        $this->providerName = $providerName;
         $this->repeat = $repeat;
         
     }
@@ -30,10 +28,22 @@ class Phone extends ChannelAbstract implements ChannelInterface
         \PHP_Timer::start();
         
         $timer = \PHP_Timer::stop();
-        $fileName = $this->path .'/'. $to. '_'.microtimer() .md5($message).'.wav' ;
+        $fileName = $this->path .'/'. $to. '_' .md5($message).'.wav' ;
 
-        Provider::GetInstanceTTS($this->providerName)->textToSpeech($message, 'M', $fileName);
+        $url = "http://10.110.255.155:9090/rest/v2/synthesize?text=ola+mundo&voice=carlos-highquality.voice";
+        
+        $client = new Client();
+        $response = $client->request('GET', $url , ['timeout' => 5]);
+        
+        if ($response->getStatusCode() == 200)
+        {
+            file_put_contents($fileName, $response->getBody());
+         }else{
+            throw new \ErrorException('Erro ao gerar TTS Cpqd.'.$text);
+        }        
 
+        shell_exec("{$this->shellScriptPath} {$this->repeat} {$fileName} {$to}");
+      
         return new ResultChannel($timer, $this->getError());
     }
 }
